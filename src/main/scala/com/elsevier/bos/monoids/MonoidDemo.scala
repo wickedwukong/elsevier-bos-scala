6a4b0dea56664edda0ea02bd6354e86531697d04
Xuemin Guan <x.guan@elsevier.com>
2016-10-24 10:32:57 +0100

    a monoid demo

--- src/main/scala/com/elsevier/bos/monoids/MonoidDemo.scala
+++ src/main/scala/com/elsevier/bos/monoids/MonoidDemo.scala
@@ -2,4 +2,27 @@ package com.elsevier.bos.monoids
 
 object MonoidDemo extends App{
 
+  val sum = List(1,2,3).foldLeft(0){
+    (acc, value) => acc + value
+  }
+
+  println(sum)
+
+  val concatenation = List("h","e","l", "l", "o").foldLeft(""){
+    (acc, value) => acc + value
+  }
+
+  println(concatenation)
+
+
+  //what about combine a list of maps?
+
+//  val mapValues: Map[String, Int] = List(Map("a" -> 1, "b" -> 2), Map("a" -> 1, "c" -> 2), Map("b" -> 3, "d" -> 0)) {
+//    (acc, map) => ???
+//  }
+//
+//
+  //Map("a" -> 2, "b" -> 5, "c" -> 2, "d" -> 0)
+//  println(mapValues)
+
 }
