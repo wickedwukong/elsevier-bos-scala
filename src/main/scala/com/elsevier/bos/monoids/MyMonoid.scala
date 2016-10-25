a5299c66f791cbaf6099a98b353854248dfd14df
Xuemin Guan <x.guan@elsevier.com>
2016-10-25 13:25:54 +0100

    implemented both string and int option monoid

--- src/main/scala/com/elsevier/bos/monoids/MyMonoid.scala
+++ src/main/scala/com/elsevier/bos/monoids/MyMonoid.scala
@@ -18,5 +18,31 @@ object MyMonoid {
 
     override def combine(t1: String, t2: String): String = t1 + t2
   }
+
+  def optionStringMonoid = new MyMonoid[Option[String]] {
+
+    override def empty: Option[String] = None
+
+    override def combine(t1: Option[String], t2: Option[String]): Option[String] = {      (t1, t2) match {
+      case (Some(x), Some(y)) => Some(stringMonoid.combine(x, y))
+      case (Some(x), None) => Some(stringMonoid.combine(x, stringMonoid.empty))
+      case (None, Some(y)) => Some(stringMonoid.combine(stringMonoid.empty, y))
+      case (None, None) => Some(stringMonoid.combine(stringMonoid.empty, stringMonoid.empty))
+    }
+    }
+  }
+
+  def optionIntMonoid = new MyMonoid[Option[Int]] {
+    override def empty: Option[Int] = None
+
+    override def combine(t1: Option[Int], t2: Option[Int]): Option[Int] = {
+      (t1, t2) match {
+        case (Some(x), Some(y)) => Some(intMonoid.combine(x, y))
+        case (Some(x), None) => Some(intMonoid.combine(x, intMonoid.empty))
+        case (None, Some(y)) => Some(intMonoid.combine(intMonoid.empty, y))
+        case (None, None) => Some(intMonoid.combine(intMonoid.empty, intMonoid.empty))
+      }
+    }
+  }
 }
 
