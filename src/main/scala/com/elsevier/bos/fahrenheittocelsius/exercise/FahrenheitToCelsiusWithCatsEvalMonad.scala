72a11d91fd042f31ab5d17b0175ecdf6dc329dda
Xuemin Guan <gxuemin@gmail.com>
2016-10-30 21:06:03 +0000

    Re-did the FahrenheitToCelsius exercise using Cats Eval monad instead of home grown IO monad

---
+++ src/main/scala/com/elsevier/bos/fahrenheittocelsius/exercise/FahrenheitToCelsiusWithCatsEvalMonad.scala
@@ -0,0 +1,27 @@
+package com.elsevier.bos.fahrenheittocelsius.exercise
+import cats.Eval
+import cats.Eval._
+
+object ConsoleUsingCatsEvalMonad {
+  def ReadLine = Eval.always{ io.StdIn.readLine() }
+  def PrintLine(message: String) = Eval.always{
+    scala.Console.println("in PrintlnLine")
+    scala.Console.println(message) }
+}
+
+object FahrenheitToCelsiusWithCatsEvalMonad extends App{
+
+  import ConsoleUsingCatsEvalMonad._
+
+  def fahrenheitToCelsius(temperatureInFahrenheit: Double) = (temperatureInFahrenheit - 32) * 5.0 / 9.0
+
+  val fahrenheitToCelsiusMonad = for {
+    _ <- PrintLine("Enter a temperature in degrees Fahrenheit:")
+    doubleValue <- ReadLine.map(_.toDouble)
+    _ <- PrintLine(fahrenheitToCelsius(doubleValue).toString)
+  } yield ()
+
+  println(fahrenheitToCelsiusMonad)
+
+  fahrenheitToCelsiusMonad.value
+}
