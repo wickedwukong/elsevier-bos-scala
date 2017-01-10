c1a8a2d139aaefc4fe4fa5f77e1d86081d55ba2c
Xuemin Guan <x.guan@elsevier.com>
2017-01-10 13:29:15 +0000

    Adding monad law

--- src/main/scala/com/elsevier/bos/monad/MonadDemo.scala
+++ src/main/scala/com/elsevier/bos/monad/MonadDemo.scala
@@ -2,13 +2,19 @@ package com.elsevier.bos.monad
 
 import cats.Functor
 
+/*
+Monad law
+1. Left identity:(pure(a) flatMapf) == f(a)
+2. Right identity:(m flatMap pure)==m
+3. Associativity: (m flatMap f flatMap g) == (m flatMap (x => f(x) flatMap g))
+*/
+
 trait Monad[F[_]] {
   def flatMap[A, B](f: A => F[B]): F[B]
   def unit[A](a: A): F[A]
 }
 
 object MonadDemo {
-
 
 }
 
