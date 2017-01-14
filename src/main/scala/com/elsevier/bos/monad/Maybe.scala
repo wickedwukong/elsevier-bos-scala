5d8e927bba7b2d09915ee7e7da1cf8bbe938f9bb
Xuemin Guan <gxuemin@gmail.com>
2017-01-14 22:11:42 +0000

    Added Maybe exercise

---
+++ src/main/scala/com/elsevier/bos/monad/Maybe.scala
@@ -0,0 +1,29 @@
+package com.elsevier.bos.monad
+
+trait Maybe[+A] {
+  def flatMap[B](f: A => Maybe[B]): Maybe[B]
+}
+
+case class Just[+A](a: A) extends Maybe[A] {
+  override def flatMap[B](f: (A) => Maybe[B]): Maybe[B] = ???
+}
+
+case object MaybeNot extends Maybe[Nothing] {
+  override def flatMap[B](f: (Nothing) => Maybe[B]): Maybe[B] = ???
+}
+
+object MaybeDemo extends App {
+
+  //Just(1) * Just(2) * Just(3)
+
+  val value: Maybe[Int] = Just(1).flatMap(a => Just(2).flatMap(b => Just(3).flatMap(c => Just(a * b))))
+
+  println(value)
+
+//  val value = for {
+//    a <- Just(1)
+//    b <- Just(2)
+//  } yield (a * b)
+//
+//  println(value)
+}
