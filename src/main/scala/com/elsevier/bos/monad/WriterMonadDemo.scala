e08eb8a51c36135c92ef418a8c9ed139a8fc5e61
Xuemin Guan <x.guan@elsevier.com>
2017-01-20 12:17:57 +0000

    added an example to show how to compse writer

--- src/main/scala/com/elsevier/bos/monad/WriterMonadDemo.scala
+++ src/main/scala/com/elsevier/bos/monad/WriterMonadDemo.scala
@@ -17,5 +17,30 @@ object WriterMonadDemo extends App {
 
   println(s"logAndValue: $logAndValue")
 
+  def composeWriter = {
+    val logAndComputation = for {
+      a <- Writer.value[Vector[String], Int](99)
+      _ <- Writer.tell(Vector("a", "b", "c"))
+      b <- Writer(Vector("x", "y", "z"), 1)
+    } yield a + b
+
+    logAndComputation
+  }
+
+  println(s"transformWriter: ${composeWriter.run}")
+
+
+  def composeWriterUsingImplicit = {
+    type Logged[A] = Writer[Vector[String], A]
+    import cats.syntax.applicative._
+
+    import cats.syntax.writer._
+
+    val writer1 = for {
+      a <- 10.pure[Logged]
+      _ <- Vector("a", "b", "c").tell
+      b <- 32.writer(Vector("x", "y", "z"))
+    } yield a + b
+  }
 
 }
