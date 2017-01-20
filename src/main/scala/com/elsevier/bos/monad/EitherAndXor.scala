4132cc2d1e27f0e858465cddbe2cd74e93f6644b
Xuemin Guan <x.guan@elsevier.com>
2017-01-20 11:13:22 +0000

    added xor exmaples

--- src/main/scala/com/elsevier/bos/monad/EitherAndXor.scala
+++ src/main/scala/com/elsevier/bos/monad/EitherAndXor.scala
@@ -55,4 +55,66 @@ object ScalazEitherDemo extends App {
   println(findDocument(UserId("99999"), "do not let me in", DocumentId("1")))
   println(findDocument(UserId("12345"), "let me in", DocumentId("100")))
 
+  val anError: Error \/ Document = -\/(AuthenticationFailed("Bad"))
+
+  val bad = anError.map{document => document.title}
+
+  println(s"bad: $bad")
+
+  val aDocument: Error \/ Document = \/-(Document(DocumentId("3"), "Latex Instructions"))
+
+  val good = aDocument.map{document => document.title}
+
+  println(s"good: $good")
+
+}
+
+
+object CatsXorDemo extends App {
+
+  import cats.data.Xor
+
+  private def documents = Map(DocumentId("1") -> "Functional programming in Scala", DocumentId("2") -> "Pragmatic Programmer")
+
+
+  private def findDocument(documentId: DocumentId): Option[Document] = {
+    documents.get(documentId).map {
+      Document(documentId, _)
+    }
+  }
+
+  private def authenticate(userId: UserId, secret: String): Xor[Error, User] = {
+    if (userId.value != "12345") {
+      Xor.left(AuthenticationFailed("Your username or password is invalid"))
+    } else {
+      Xor.right(User(userId, "Bob"))
+    }
+  }
+
+
+  def findDocument(userId: UserId, secret: String, documentId: DocumentId): Error Xor Document = {
+    import cats.implicits._
+
+    for {
+      _ <- authenticate(userId, secret)
+      document <- findDocument(documentId).toRightXor(InvalidDocument(documentId, s"No document found for document Id: ${documentId.value}"))
+    } yield document
+  }
+
+  println(findDocument(UserId("12345"), "let me in", DocumentId("1")))
+  println(findDocument(UserId("99999"), "do not let me in", DocumentId("1")))
+  println(findDocument(UserId("12345"), "let me in", DocumentId("100")))
+
+  val anError: Xor[Error,Document] = Xor.left(AuthenticationFailed("Bad"))
+
+  val bad = anError.map{document => document.title}
+
+  println(s"bad: $bad")
+
+  val aDocument: Xor[Error, Document] = Xor.right(Document(DocumentId("3"), "Latex Instructions"))
+
+  val good = aDocument.map{document => document.title}
+
+  println(s"good: $good")
+
 }
